088d362e11dd8d31ccbb3f9b1640634c6e08e636
Arkokhan21 <khanarko2021@gmail.com>
2022-08-22 17:42:31 +0600

    added design

--- js/selected.js
+++ js/selected.js
@@ -10,13 +10,17 @@ function getPlayerName(name) {
 function btnDisabled(btn) {
     const button = document.getElementById(btn)
     button.setAttribute('disabled', true)
+    button.style.backgroundColor = 'gray'
 }
 
 function getLi() {
     const list = document.getElementById("ol").getElementsByTagName("li")
     const result = list.length
     if (result > 5) {
-        alert('Sorry! You cannot select more than five')
+        alert('Sorry! you cannot select more than five players')
+        const ol = document.getElementById('ol')
+        ol.removeChild(ol.lastChild)
+        return;
     }
     return result
 }
@@ -26,26 +30,31 @@ document.getElementById('btn-1').addEventListener('click', function () {
     btnDisabled('btn-1')
     getLi()
 })
+
 document.getElementById('btn-2').addEventListener('click', function () {
     getPlayerName('messi')
     btnDisabled('btn-2')
     getLi()
 })
+
 document.getElementById('btn-3').addEventListener('click', function () {
     getPlayerName('neymar')
     btnDisabled('btn-3')
     getLi()
 })
+
 document.getElementById('btn-4').addEventListener('click', function () {
     getPlayerName('ramos')
     btnDisabled('btn-4')
     getLi()
 })
+
 document.getElementById('btn-5').addEventListener('click', function () {
     getPlayerName('ronaldo')
     btnDisabled('btn-5')
     getLi()
 })
+
 document.getElementById('btn-6').addEventListener('click', function () {
     getPlayerName('ronato')
     btnDisabled('btn-6')
